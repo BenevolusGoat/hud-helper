a44cff7df9a61f87cce2361bdad62073679b824c
BenevolusGoat <brude602@gmail.com>
2025-02-21 18:05:27 -0500

    Update version

--- src_hudhelper/hud_helper.lua
+++ src_hudhelper/hud_helper.lua
@@ -1,7 +1,7 @@
 local Mod = HudHelperExample
 local emptyShaderName = "HudHelperEmptyShader"
 
-local VERSION = 1.12 -- (v1.1.2) do not modify
+local VERSION = 1.2 -- (v1.2) do not modify
 local game = Game()
 
 -- debug
